24b8d5daaa08a7894205b612710a41d0752aed42
Willian-code <willianrosendodelima@outlook.com>
2022-09-27 11:48:28 -0300

    dino JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 import { updateGround, setupGround } from './ground.js'
+import { updateDino, setupDino } from './dino.js'
 
 const	WORLD_WIDTH = 100;
 const	WORLD_HEIGHT = 30;
@@ -25,6 +26,7 @@ function update(time) {
 	const	delta = time - lastTime;
 
 	updateGround(delta, speedScale);
+	updateDino(delta, speedScale);
 	updateSpeedScale(delta);
 	updateScore(delta);
 
@@ -46,6 +48,7 @@ function handleStart() {
 	speedScale = 1;
 	score = 0;
 	setupGround();
+	setupDino();
 	startScreenElem.classList.add('hide');
 	window.requestAnimationFrame(update);
 }
